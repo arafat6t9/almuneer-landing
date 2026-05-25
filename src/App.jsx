df0ae71f2a9feb40f3fb3b197a33e811ad1dfaf3
arafat6t9 <arafat6t9@gmail.com>
2026-05-25 14:40:58 +0600

    Added floating WhatsApp support

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,47 @@ import ComingSoon from "./sections/ComingSoon";
 import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
 
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+
+import logo from "./assets/logo.png";
+
 function App() {
+
   return (
-    <div className="overflow-hidden">
+
+    <div className="overflow-hidden bg-white">
+
       <Navbar />
+
       <Hero />
+
       <About />
+
       <Products />
+
       <ComingSoon />
+
       <Contact />
+
       <Footer />
+
+      {/* WHATSAPP FLOAT */}
+      <FloatingWhatsApp
+        phoneNumber="+971568994436"
+        accountName="Al Muneer Trading LLC"
+        avatar={logo}
+        statusMessage="Typically replies instantly"
+        chatMessage="Hello 👋\nHow can we help you today?"
+        placeholder="Type your message..."
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound
+        darkMode
+      />
+
     </div>
+
   );
 }
 
